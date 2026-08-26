72a99a1b9b39f43345866f5f21ad87aa199a45be
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-26 22:57:56 +0200

    added the solution for adding two linked lists of the same size

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -1,11 +1,28 @@
+#!/bin/python3
 """
 2. Add Two Numbers
-https://leetcode.com/problems/add-two-numbers/
+https://leetcode.com/problems/add-two-number
 """
-
 class Solution:
     def solve(self, l1, l2):
-        raise NotImplementedError
+        head = ListNode()
+        current = heas
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            current.next = ListNode(total%10)
+            current = current.next
+            carry = total//10
+
+            if l1 :
+                l1 =l1.next
+            if l2 :
+                l2 = l2.next
+        return head.next1
 
 
 if __name__ == "__main__":
@@ -22,3 +39,4 @@ if __name__ == "__main__":
         assert result == expected, f"solve{args} = {result}, expected {expected}"
 
     print("All tests passed.")
+
