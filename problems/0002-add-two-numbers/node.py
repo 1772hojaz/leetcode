6424c0d2197bbfafb573e6b340a00fc8d7925a31
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-27 22:47:07 +0200

    improved documentation

--- problems/0002-add-two-numbers/node.py
+++ problems/0002-add-two-numbers/node.py
@@ -1,11 +1,24 @@
 #!/bin/python3
 
 class ListNode():
+    """
+    the class of a linked list
+    Attributes:
+        val: the value of the node
+        next: the pointer of the node
+    """
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
     def list_to_linked(values):
+        """
+        makes an array into a linked list
+        Args:
+            values: the array
+        Retutrns:
+            dummy.next:the linked list
+        """
         dummy = ListNode()
         current = dummy
         for v in values:
@@ -14,6 +27,13 @@ class ListNode():
         return dummy.next
 
     def linked_to_list(node):
+        """
+        changes a linked list into an array
+        Args:
+            node: the linked list
+        Returns:
+            results: the array
+        """
         result = []
         while node:
             result.append(node.val)
