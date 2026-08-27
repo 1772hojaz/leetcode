5fd9bf47b0546beab79cdd32f4226671595c7224
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-27 22:02:27 +0200

    imported the node module

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -1,7 +1,8 @@
 #!/bin/python3
+from node import ListNode
 
 class Solution:
-    def solve(l1,l2):
+    def solve(self, l1,l2):
         dummy_head = ListNode()
         current = dummy_head
         carry = 0
@@ -21,7 +22,7 @@ class Solution:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
-        return head.next
+        return dummy_head.next
 
 if __name__ == "__main__":
     sol = Solution()
@@ -33,9 +34,9 @@ if __name__ == "__main__":
     ]
 
     for args, expected in tests:
-        l1, l2 = [L.list_to_linked(a) for a in args]
+        l1, l2 = [ListNode.list_to_linked(a) for a in args]
         result = sol.solve(l1, l2)
-        result_list = L.linked_to_list(result)
+        result_list = ListNode.linked_to_list(result)
         assert result_list == expected, f"solve{args} = {result_list}, expected {expected}"
 
     print("All tests passed.")
