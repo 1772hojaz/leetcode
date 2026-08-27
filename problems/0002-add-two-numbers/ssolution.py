779e59034060e24e9b8f434ecd3dacb0c8985c5c
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-27 22:32:43 +0200

    added the solution from yesterday

--- problems/0002-add-two-numbers/ssolution.py
+++ problems/0002-add-two-numbers/ssolution.py
@@ -24,23 +24,3 @@ class Solution:
             if l2 :
                 l2 = l2.next
         return head.next
-
-
-
-if __name__ == "__main__":
-    sol = Solution()
-
-    tests = [
-        (([2, 4, 3], [5, 6, 4]), [7, 0, 8]),
-        (([0], [0]), [0]),
-        (([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]), [8, 9, 9, 9, 0, 0, 0, 1]),
-    ]
-
-    for args, expected in tests:
-        l1, l2 = [L.list_to_linked(a) for a in args]
-        result = sol.solve(l1, l2)
-        result_list = L.linked_to_list(result)
-        assert result_list == expected, f"solve{args} = {result_list}, expected {expected}"
-
-    print("All tests passed.")
-
