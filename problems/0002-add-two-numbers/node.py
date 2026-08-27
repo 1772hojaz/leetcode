d850b21297d45010231ac63b32ff608adbd5b8b2
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-27 22:12:10 +0200

    added a seperate test case

--- problems/0002-add-two-numbers/node.py
+++ problems/0002-add-two-numbers/node.py
@@ -4,3 +4,20 @@ class ListNode():
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    def list_to_linked(values):
+        dummy = ListNode()
+        current = dummy
+        for v in values:
+            current.next = ListNode(v)
+            current = current.next
+        return dummy.next
+    
+    def linked_to_list(node):
+        result = []
+        while node:
+            result.append(node.val)
+            node = node.next
+
+        return result
+
