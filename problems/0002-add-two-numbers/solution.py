20b3631cb927daa78b3bdd69fc3341a8dc379044
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-08-27 22:27:21 +0200

    improved the modular design

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -23,21 +23,3 @@ class Solution:
             if l2:
                 l2 = l2.next
         return dummy_head.next
-
-if __name__ == "__main__":
-    sol = Solution()
-
-    tests = [
-        (([2, 4, 3], [5, 6, 4]), [7, 0, 8]),
-        (([0], [0]), [0]),
-        (([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]), [8, 9, 9, 9, 0, 0, 0, 1]),
-    ]
-
-    for args, expected in tests:
-        l1, l2 = [ListNode.list_to_linked(a) for a in args]
-        result = sol.solve(l1, l2)
-        result_list = ListNode.linked_to_list(result)
-        assert result_list == expected, f"solve{args} = {result_list}, expected {expected}"
-
-    print("All tests passed.")
-
