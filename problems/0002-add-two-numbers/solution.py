3dc7cdc55f45995250ccbea8f63ce8de8bff7e4e
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-09-03 13:52:34 +0200

    added the linnked list additiion

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -1,5 +1,6 @@
 #!/bin/python3
-"
+from node import ListNode as LinkedList
+"""
 This is a solution for adding two linked lists.
 Args:
     l1: is the first linked list
@@ -7,21 +8,28 @@ Args:
 
 Return:
     head.next: Is the head of the actual linked list solution because head is the dummy one
-"
+"""
+
 class Solution:
     def add(self, l1, l2):
-        dumy_node = LinkedList()
-        head = dummy_node
+        head = LinkedList()
+        current = head
         carry = 0
 
         while l1 or l2 or carry:
-            x  = l1.value else 0
-            y = l2.value else 0
+            x  = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
             total = x + y + carry
-
-            head.next = LinkedList(total%10)
-            head = head.next
-
             carry = total//10
+
+            current.next = LinkedList(total%10)
+            current = current.next
+
+            #Do not forget the to change the linked lists that ou are adding
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
         return head.next
